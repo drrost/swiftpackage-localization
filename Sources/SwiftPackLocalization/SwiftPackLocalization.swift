e626dd4aef2b7f9e4d0333e8ca361b31f6adb00f
Rostyslav Druzhchenko <rdruzhchenko@teamworkcommerce.com>
2020-11-28 13:30:54 +0200

    Add basic strings localization example

--- Sources/SwiftPackLocalization/SwiftPackLocalization.swift
+++ Sources/SwiftPackLocalization/SwiftPackLocalization.swift
@@ -1,3 +1,58 @@
-struct SwiftPackLocalization {
-    var text = "Hello, World!"
+//
+//  ErrorFactory.swift
+//
+//
+//  Created by Rostyslav Druzhchenko on 28.11.2020.
+//
+
+import Foundation
+
+public class CustomError: Error {
+
+    let code: Int
+    let message: String
+
+    public init(_ code: Int, _ message: String) {
+        self.code = code
+        self.message = message
+    }
+}
+
+public class LanguageManager {
+
+    static let shared = LanguageManager()
+
+    public var language: String = "en"
+
+    private init() {}
+}
+
+func L(_ key: String, _ language: String) -> String {
+    let path = Bundle.module.path(forResource: language, ofType: "lproj")
+    let bundle = Bundle(path: path!)
+    return bundle!.localizedString(forKey: "ERR_MESSAGE_CONNECTION_LOST", value: nil, table: nil)
+}
+
+public class ErrorFactory {
+
+    let language: String
+
+    public init() {
+        self.language = LanguageManager.shared.language
+    }
+
+    func error(for code: Int) -> CustomError {
+
+        var message: String
+        switch code {
+        case 1:
+            message = L("ERR_MESSAGE_CONNECTION_LOST", language)
+        case 2:
+            message = L("ERR_MESSAGE_TRNASACTION_FAILED", language)
+        default:
+            message = L("ERR_MESSAGE_UNKNOWN", language)
+        }
+
+        return CustomError(code, message)
+    }
 }
--- Tests/SwiftPackLocalizationTests/SwiftPackLocalizationTests.swift
+++ Tests/SwiftPackLocalizationTests/SwiftPackLocalizationTests.swift
@@ -1,15 +1,42 @@
+//
+//  SwiftPackLocalization.swift
+//
+//
+//  Created by Rostyslav Druzhchenko on 28.11.2020.
+//
+
 import XCTest
+
 @testable import SwiftPackLocalization
 
 final class SwiftPackLocalizationTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftPackLocalization().text, "Hello, World!")
+
+    func testConnectionLostEn() {
+        // Given
+        let code = 1
+        LanguageManager.shared.language = "en"
+        let errorFactory = ErrorFactory()
+
+        // When
+        let error = errorFactory.error(for: code)
+
+        // Then
+        XCTAssertEqual(1, error.code)
+        XCTAssertEqual("Connection lost", error.message)
     }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    func testConnectionLostFr() {
+        // Given
+        let code = 1
+        LanguageManager.shared.language = "fr"
+        let errorFactory = ErrorFactory()
+
+        // When
+        let error = errorFactory.error(for: code)
+
+        // Then
+        XCTAssertEqual(1, error.code)
+        XCTAssertEqual("Connexion perdue", error.message)
+    }
+
 }
